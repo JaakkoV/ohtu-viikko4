04bbaf0e51c076c13c3d2316c30567e1c4b405ff
JaakkoV <jaakvirt@cs.helsinki.fi>
2017-04-07 16:56:26 +0300

    Exercise 1 done

--- src/main/java/ohtu/Main.java
+++ src/main/java/ohtu/Main.java
@@ -1,32 +1,43 @@
 package ohtu;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.apache.http.client.fluent.Request;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         // vaihda oma opiskelijanumerosi seuraavaan, ÄLÄ kuitenkaan laita githubiin omaa opiskelijanumeroasi
-        String studentNr = "01234567";
-        if ( args.length>0) {
+        String studentNr = "whoEvenCares";
+        if (args.length > 0) {
             studentNr = args[0];
         }
 
-        String url = "http://ohtustats2017.herokuapp.com/students/"+studentNr+"/submissions";
+        String url = "http://ohtustats2017.herokuapp.com/students/" + studentNr + "/submissions";
 
         String bodyText = Request.Get(url).execute().returnContent().asString();
 
-        System.out.println("json-muotoinen data:");
-        System.out.println( bodyText );
-
+//        System.out.println("json-muotoinen data:");
+//        System.out.println(bodyText);
         Gson mapper = new Gson();
         Submission[] subs = mapper.fromJson(bodyText, Submission[].class);
-        
-        System.out.println("Oliot:");
-        for (Submission submission : subs) {
-            System.out.println(submission);
-        }
 
+        System.out.println("Kurssi: Ohjelmistotuotanto, kevät 2017");
+        System.out.println(String.format("opiskelijanumero: %s", subs[0].getStudent_number()));
+        System.out.println("**************");
+        int hoursTotal = 0;
+        int exercisesDone = 0;
+
+        for (Submission submission : subs) {
+            submission.printPretty();
+            hoursTotal += submission.getHours();
+            exercisesDone += submission.getExercisesDone();
+        }
+        System.out.println("");
+        System.out.println(String.format("yhteensä: %d tehtävää %d tuntia", exercisesDone, hoursTotal));
     }
 }
